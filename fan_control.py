9280684244b7b86cd5eef01971ca56e58ba355e6
Marc Venturini <marc.venturini@gmail.com>
2021-08-26 23:25:21 +0800

    Renamed temperature to temp for conciseness

--- fan_control.py
+++ fan_control.py
@@ -7,13 +7,13 @@ import time
 THERMAL_ZONE = 0
 PWM_CHIP = 0
 PWM_CHANNEL = 0
-MIN_TEMPERATURE = 40
-MAX_TEMPERATURE = 55
+MIN_TEMP = 40
+MAX_TEMP = 55
 MIN_SPEED = 30
 MAX_SPEED = 100
 
 
-def get_temperature() -> float:
+def read_temp() -> float:
     """Reads the temperature."""
     with open("/sys/class/thermal/thermal_zone%d/temp" % THERMAL_ZONE) as f:
         return int(f.readline()) / 1000
@@ -60,30 +60,30 @@ class PWM:
 class Controller:
     """Converts temperatures to fan speeds. Uses a linear saturated fan curve."""
 
-    def __init__(self, min_temperature: float, max_temperature: float, min_speed: float, max_speed: float) -> None:
-        assert max_temperature > min_temperature
+    def __init__(self, min_temp: float, max_temp: float, min_speed: float, max_speed: float) -> None:
+        assert max_temp > min_temp
         assert max_speed > min_speed
-        self._min_temperature = min_temperature
-        self._max_temperature = max_temperature
+        self._min_temp = min_temp
+        self._max_temp = max_temp
         self._min_speed = min_speed
         self._max_speed = max_speed
-        self._ratio = (max_speed - min_speed) / (max_temperature - min_temperature)
+        self._ratio = (max_speed - min_speed) / (max_temp - min_temp)
 
-    def get_speed(self, temperature: float) -> float:
+    def get_speed(self, temp: float) -> float:
         """Converts a temperature to a fan speed."""
-        if temperature < self._min_temperature:
+        if temp < self._min_temp:
             return self._min_speed
-        if temperature > self._max_temperature:
+        if temp > self._max_temp:
             return self._max_speed
-        return self._min_speed + self._ratio * (temperature - self._min_temperature)
+        return self._min_speed + self._ratio * (temp - self._min_temp)
 
 
 if __name__ == "__main__":
-    controller = Controller(MIN_TEMPERATURE, MAX_TEMPERATURE, MIN_SPEED, MAX_SPEED)
+    controller = Controller(MIN_TEMP, MAX_TEMP, MIN_SPEED, MAX_SPEED)
     pwm = PWM(chip=PWM_CHIP, channel=PWM_CHANNEL, period=40000)
     pwm.init()
     while True:
-        temperature = get_temperature()
-        percent = controller.get_speed(temperature)
+        temp = read_temp()
+        percent = controller.get_speed(temp)
         pwm.update(percent)
         time.sleep(1)
